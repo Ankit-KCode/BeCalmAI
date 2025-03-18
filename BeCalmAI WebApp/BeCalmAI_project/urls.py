acdddbc9c2c5879da29aba042ad8ce29712dd57a
Ankit Kumar <kumarankit802215@gmail.com>
2025-03-18 23:18:24 +0530

    BeCalmAI - Version 0.1

--- BeCalmAI WebApp/BeCalmAI_project/urls.py
+++ BeCalmAI WebApp/BeCalmAI_project/urls.py
@@ -1,7 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
+from BeCalmAI.views import chat_view, chat_api
 
 urlpatterns = [
+    path('', chat_view, name='chat'),
     path('admin/', admin.site.urls),
+    path('BeCalmAI/', chat_view, name='chat'),
+    path('chat-api/', chat_api, name='chat_api'),  # API for chat responses
 ]
